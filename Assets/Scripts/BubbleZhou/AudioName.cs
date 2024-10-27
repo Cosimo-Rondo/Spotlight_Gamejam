df556af295af7977f30fd9fed47b547cf27df037
李禹虬 <278275902@qq.com>
2024-10-28 03:42:44 +0800

    音频系统和部分音频素材
    阿巴阿巴？

---
+++ Assets/Scripts/BubbleZhou/AudioName.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//存储所有的音频名称
+public static class AudioName
+{
+
+    public static string BGM0 { get => "BGM_0"; }
+    public static string[] BGM1_1 { get => new string[1] { "BGM_1_3_2" }; }
+    /*// 测试BGM
+    public static string BGM2 { get => "BGM2"; }
+    public static string BOSS1BGM { get => "BOSS1BGM"; }
+
+    // 跳跃落地声1-3
+    public static string[] _fallToGround { get => new string[3] { "Splat 1", "Splat 2", "Splat 3" }; }
+    public static string[] _footstepClips { get => new string[2] { "Footstep 1", "Footstep 2" }; }
+    public static string[] _wallClimbClips { get => new string[2] { "Wall Climb 1", "Wall Climb 2" }; }
+    public static string[] _ladderClimbClips { get => new string[2] { "Ladder Climb 1", "Ladder Climb 2" }; }
+    public static string _dashClip { get => "Dash"; }
+    // public static string _landClip { get => "Landing"; }
+    public static string _jumpClip { get => "Jump"; }
+    public static string _doubleJumpClip { get => "Double Jump"; }
+    public static string _attackClip { get => "Attack"; }
+    public static string _absorbClip { get => "Absorb"; }
+    public static string _beattackedClip { get => "Beattacked"; }
+    public static string _diedClip { get => "Died"; }
+    public static string _enemyBeattakedClip { get => "EnemyBeattacked"; }
+    public static string _enemyDiedClip { get => "EnemyDied"; }
+    public static string _enemyReactClip { get => "EnemyReact"; }
+    public static string[] _enemyAttackClips { get => new string[4] { "EnemyAttack1", "EnemyAttack2", "EnemyAttack3", "EnemyAttack4" }; }
+    public static string _attackToBarrier { get => "AttackToBarrier"; }
+    public static string _bossGrowl { get => "BOSSGrowl"; }
+    public static string _bossClap { get => "BOSSClap"; }
+    public static string _bossLash { get => "BOSSLash"; }
+    public static string _bossThrow { get => "BOSSThrow"; }
+    public static string _bossLand { get => "BOSSLand"; }
+    public static string _stone { get => "StoneSui"; }*/
+}
