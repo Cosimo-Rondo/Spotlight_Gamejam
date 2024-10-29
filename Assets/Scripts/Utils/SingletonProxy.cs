0a05b89b04dc0ccfb8ff2cbae20197cb99abcb53
Bubble <bubble327670@gmail.com>
2024-10-29 20:46:18 +0800

    singletonProxy

--- Assets/Scripts/Utils/SingletonProxy.cs
+++ Assets/Scripts/Utils/SingletonProxy.cs
@@ -4,5 +4,16 @@ using UnityEngine;
 
 public class SingletonProxy : MonoBehaviour
 {
-    
+    public void PlayBGM(string name)
+    {
+        AudioManager.PlayBGM(name);
+    }
+    public void PlaySFXNoWait(string name)
+    {
+        AudioManager.PlaySFXNoWait(name);
+    }
+    public void PlaySFXWait(string name)
+    {
+        AudioManager.PlaySFXWait(name);
+    }
 }
