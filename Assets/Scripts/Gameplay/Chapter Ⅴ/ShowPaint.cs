95c10bfda7607b7ae69f2dd24641b3f9784b2f80
Atuia <1399393784@qq.com>
2024-10-29 18:04:30 +0800

    Pieces Smooth Show

--- Assets/Scripts/Gameplay/Chapter Ⅴ/ShowPaint.cs
+++ Assets/Scripts/Gameplay/Chapter Ⅴ/ShowPaint.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class ShowPaint : MonoBehaviour
 {
     public bool canShow=false;
+    public bool show_over = false;
     public GameObject line;
+    private float duration = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,13 +20,31 @@ public class ShowPaint : MonoBehaviour
         
     }
     public void Show(){
-        if(canShow)
+        if (canShow)
+        {
             gameObject.SetActive(true);
+            StartCoroutine(SmoothShowPieces());
+        }
     }
     public void CanShow(){
         if(line.activeSelf)
             canShow=true;
         else
             canShow=false;
+    }
+
+    IEnumerator SmoothShowPieces()
+    {
+        Material piece_mat = transform.GetComponent<SpriteRenderer>().material;
+        piece_mat.color = new Color(1, 1, 1, 0);
+        float timer = 0;
+        while (timer < duration)
+        {
+            timer += Time.deltaTime;
+            float t = Mathf.Clamp(timer / duration, 0, 1);
+            piece_mat.color = Color.Lerp(new Color(1, 1, 1, 0), new Color(1, 1, 1, 1), t);
+            yield return null;
+        }
+        show_over = true;
     }
 }
